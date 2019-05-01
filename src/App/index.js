c12a2ea2c94ad37016ca2f94b37f61a95fc86584
Changhyun Jeong <chjeong@e1z2r3p20.42.us.org>
2019-05-01 13:16:13 -0700

    Addtind Styled button

---
+++ src/App/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Welcome from './WelcomeMessage';
+import styled, {css} from 'styled-components';
+
+const MyButton = styled.div`
+  color:green;
+  ${props => props.primary && css`      
+      color: palevioletred;
+    `}
+`
+
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
+
+
+function App() {
+  return (
+    <div>
+      <Welcome/>      
+      <MyButton> Hello </MyButton>
+      <MyButton primary> Hello </MyButton>
+      <TomatoButton primary> Hello </TomatoButton>
+    </div>
+  );
+}
+
+export default App;
