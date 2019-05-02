260a8f0a281bad5b8cfa6af5425b57250934b98d
Changhyun Jeong <chjeong@e1z2r3p20.42.us.org>
2019-05-01 18:14:47 -0700

    Add AppProvider and hook up to AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
-import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from 'constants';
+import { AppContext } from './AppProvider';
 
 const Logo = styled.div`
     font-size: 1.5em;
 `
-
 const Bar = styled.div`
     display: grid;
     margin-bottom: 40px
     grid-template-columns: 180px auto 100px 180px;
 `
-
-
 
 const ControlButtonElem = styled.div`
     cursor: pointer;
@@ -24,17 +21,27 @@ function toProperCase(lower){
     return lower.charAt(0).toUpperCase() + lower.substr(1);
 }
 
-function ControlButton({name, active}){
-    return <ControlButtonElem active = {active}>
-         {toProperCase(name)}
-        </ControlButtonElem>
+function ControlButton({name}){
+    return (
+        <AppContext.Consumer>
+            {({page, setPage}) => (
+                <ControlButtonElem
+                   active = {page === name}
+                   onClick={() => setPage(name)}
+                >
+                    {toProperCase(name)}
+                </ControlButtonElem>
+            )}
+        </AppContext.Consumer>
+    )
+    
 }
 
 
 export default function(){
     return(
         <Bar>
-            <Logo> CrytoDash </Logo>
+            <Logo> CryptoDash </Logo>
             <div/>
             <ControlButton active name = "dashboard"/>
             <ControlButton name = "setting"/>
--- src/App/index.js
+++ src/App/index.js
@@ -4,12 +4,16 @@ import Welcome from './WelcomeMessage';
 import styled, {css} from 'styled-components';
 import AppLayout from './AppLayout'
 import AppBar from './AppBar';
+import {AppProvider} from './AppProvider';
 
 function App() {
   return (
     <AppLayout>
-      <AppBar/>
-      <Welcome/>                        
+      <AppProvider>
+        <AppBar />
+        <Welcome />
+      </AppProvider>
+      
     </AppLayout>
   );
 }
