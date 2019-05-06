554aa87811c7c7f5e394ace895ff975fb91ae922
Changhyun Jeong <chjeong@e1z2r3p20.42.us.org>
2019-05-05 22:00:20 -0700

    Add hights theme

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -19,7 +19,7 @@ export default function(){
             <PriceGrid />
             <ChartGird>
             <CoinSpotLight />             
-            <PriceChart/>
+            <PriceChart/ >
             </ChartGird>
         </Page>
     )
