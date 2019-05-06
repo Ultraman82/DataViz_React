376fda2950555d2313164a7572a6f6092432eb7b
Changhyun Jeong <chjeong@e1z2r3p20.42.us.org>
2019-05-05 21:23:44 -0700

    Add ConSpotlight

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 import Page from '../Shared/Page';
 import PriceGrid from './PriceGrid';
+import CoinSpotLight from "./CoinSpotLight"
+import styled from "styled-components";
 
+
+
+const ChartGird = styled.div`
+    display: grid;
+    margin-top: 20px;
+    grid-gap: 15px;
+    grid-template-columns: 1fr 3fr;    
+`
 export default function(){
     return (
         <Page name = "dashboard">
             <PriceGrid />
+            <ChartGird>
+            <CoinSpotLight />             
+            <div> char goes here </div>
+            </ChartGird>
         </Page>
     )
 }
--- src/Shared/CoinImage.js
+++ src/Shared/CoinImage.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import styled, {css}  from 'styled-components';
 
-export default function ({coin, style}) {
-    return (
-        <img
-            alt={coin.CoinSymbol}
-            style={style || {height: '50px'}}
+const CoinImage = styled.img`
+    height: 50px;
+    ${props => props.spotlight && css`
+        height: 200px;
+        margin: auto;
+        display: block;
+    `}
+`
+
+
+export default function({coin, spotlight}) {    
+    return(
+        <CoinImage
+        spotlight={spotlight}
+            alt={coin.CoinSymbol}            
             src={`http://cryptocompare.com/${
-            coin.ImageUrl
-            }`}
+                coin.ImageUrl}
+                `}
         />
     );
 }
+
+
+
