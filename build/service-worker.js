6500760d8648e22a902c0f8880392ad43bd553f7
Changhyun Jeong <chjeong@e1z2r3p22.42.us.org>
2019-05-23 04:32:37 -0700

    Push#2

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c0833a9763d41f38b0807ed1d0a093c.js"
+  "https//Ultraman82.github.io/DataViz_React/precache-manifest.1eae8384895da0c0659ee100e1c08c58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https//Ultraman82.github.io/DataViz_React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
