afa24fe57664900819b6de40e1b8dc6372300516
Changhyun Jeong <chjeong@e1z2r3p22.42.us.org>
2019-05-23 04:32:49 -0700

    Updates

---
+++ precache-manifest.1eae8384895da0c0659ee100e1c08c58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4713ce834bd14d98f84caa6538025c58",
+    "url": "https//Ultraman82.github.io/DataViz_React/index.html"
+  },
+  {
+    "revision": "6d32fe8f8b9a84b24e83",
+    "url": "https//Ultraman82.github.io/DataViz_React/static/css/main.55fd877a.chunk.css"
+  },
+  {
+    "revision": "d22881103df9f1fa2bfe",
+    "url": "https//Ultraman82.github.io/DataViz_React/static/js/2.d4bedf72.chunk.js"
+  },
+  {
+    "revision": "6d32fe8f8b9a84b24e83",
+    "url": "https//Ultraman82.github.io/DataViz_React/static/js/main.f3475cd5.chunk.js"
+  },
+  {
+    "revision": "ba809bd6dd99e118d1e4",
+    "url": "https//Ultraman82.github.io/DataViz_React/static/js/runtime~main.bc2631a1.js"
+  }
+]);
